efeeac34ec6feb825a28d07eb0244fda3c8dbcea
ThiNguyen <nguyenthisc1@gmail.com>
2022-06-13 14:35:39 +0700

    rebuild app

--- login_firebase_app/lib/main.dart
+++ login_firebase_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,105 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Firebase',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage("assets/bg-3.webp"), fit: BoxFit.cover),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: LoginScreen(),
-      ),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  LoginScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      padding: const EdgeInsets.all(16),
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: size.height * 0.4,
-            child: Text("Login App",
-                style: GoogleFonts.lobster(
-                    color: Colors.white,
-                    letterSpacing: 2,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold)),
+        title: 'Login Firebase',
+        home: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/bg-4.webp"), fit: BoxFit.cover),
           ),
-          Container(
-            child: Column(children: [
-              Container(
-                  width: 200,
-                  height: 50,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                    ),
-                    onPressed: () {},
-                    child: Text('Sign Up'.toUpperCase(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                  )),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                      ),
-                      borderRadius: BorderRadius.circular(2)),
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                    ),
-                    onPressed: () {},
-                    child: Text('Login'.toUpperCase(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        )),
-                  ))
-            ]),
-          )
-        ],
-      ),
-    );
+          child:
+              Scaffold(backgroundColor: Colors.transparent, body: Text("123")),
+        ));
   }
 }
